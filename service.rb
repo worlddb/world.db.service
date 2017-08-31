da21db26f2d6966ee607058c211a627ea1c4c457
Gerald Bauer <gerald.bauer@gmail.com>
2017-08-31 20:17:02 +0200

    Create service.rb

---
+++ service.rb
@@ -0,0 +1,17 @@
+
+get '/countries(.:format)?' do
+  Country.by_key.all    # sort/order by key
+end
+
+get '/cities(.:format)?' do
+  City.by_key.all       # sort/order by key
+end
+
+get '/continents(.:format)?' do
+  Continent.all
+end
+
+
+get '/tag/:slug(.:format)?' do      # e.g. /tag/north_america.csv
+  Tag.find_by!( slug: params['slug'] ).countries
+end
